88d2ecca58e6e7493fdaec2607765e259d7616f0
geofflorne <geo.lorne@gmail.com>
2016-10-03 23:22:02 -0700

    all done

--- assign1/proc.h
+++ assign1/proc.h
@@ -1,106 +1,102 @@
-typedef struct proc{
-    pid_t pid;
-    char *cmd;
-    int isStop;
-    struct proc *next;
-} proc;
+typedef struct proc {
+  pid_t pid;
+  char * cmd;
+  int isStop;
+  struct proc * next;
+}proc;
 
-proc *head = NULL;
-proc *curr = NULL;
+proc * head = NULL;
+proc * curr = NULL;
 int count = 0;
 
-proc* add(pid_t pid, char *cmd, int isStop){
-    proc *ptr = (proc*)malloc(sizeof(proc));
-    if(ptr == NULL){
-        printf("malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+proc * add(pid_t pid, char * cmd, int isStop) {
+  proc * ptr = (proc * ) malloc(sizeof(proc));
+  if (ptr == NULL) {
+    printf("malloc failed\n");
+    exit(EXIT_FAILURE);
+  }
 
-    ptr->pid = pid;
-    ptr->cmd = strdup(cmd);
-    ptr->isStop = isStop;
-    ptr->next = NULL;
+  ptr->pid = pid;
+  ptr->cmd = strdup(cmd);
+  ptr->isStop = isStop;
+  ptr->next = NULL;
 
-    if(head == NULL){
-      head = curr = ptr;
-    }else{
-      curr->next = ptr;
-      curr = ptr;
-    }
-    count++;
-    return ptr;
+  if (head == NULL) {
+    head = curr = ptr;
+  } else {
+    curr->next = ptr;
+    curr = ptr;
+  }
+  count++;
+  return ptr;
 }
 
-
-proc* find(pid_t pid, proc ** prev){
-    proc *ptr = head;
-    proc *tmp = NULL;
-    int found = 0;
-    while(ptr != NULL){
-      if(ptr->pid == pid){
-          found = 1;
-          break;
-      }else{
-        tmp = ptr;
-        ptr = ptr->next;
-      }
+proc * find(pid_t pid, proc * * prev) {
+  proc * ptr = head;
+  proc * tmp = NULL;
+  int found = 0;
+  while (ptr != NULL) {
+    if (ptr->pid == pid) {
+      found = 1;
+      break;
+    } else {
+      tmp = ptr;
+      ptr = ptr->next;
     }
-    if(found){
-      if(prev){
-        *prev = tmp;
-      }
+  }
+  if(found){
+      if(prev) *prev = tmp;
       return ptr;
     }else{
-      return NULL;
-    }
+    return NULL;
+  }
 }
 
-int delete(pid_t pid){
-  proc *prev = NULL;
-  proc *del = NULL;
+int delete(pid_t pid) {
+  proc * prev = NULL;
+  proc * del = NULL;
 
-  del = find(pid, &prev);
+  del = find(pid, & prev);
 
-  if(del == NULL){
-        return -1;
-    }else{
-        if(prev != NULL)
-            prev->next = del->next;
+  if (del == NULL) {
+    return -1;
+  } else {
+    if (prev != NULL)
+      prev->next = del->next;
 
-        if(del == curr){
-            curr = prev;
-        }
-        if(del == head){
-            head = del->next;
-        }
+    if (del == curr) {
+      curr = prev;
     }
+    if (del == head) {
+      head = del->next;
+    }
+  }
 
-    free(del);
-    del = NULL;
-    count--;
-    return 0;
+  free(del);
+  del = NULL;
+  count--;
+  return 0;
 }
 
-void set_isStop(pid_t pid, int isStop){
-  proc *ptr = head;
-  while(ptr!=NULL){
-    if(ptr->pid == pid){
+void set_isStop(pid_t pid, int isStop) {
+  proc * ptr = head;
+  while (ptr != NULL) {
+    if (ptr->pid == pid) {
       ptr->isStop = isStop;
       break;
-    }else{
+    } else {
       ptr = ptr->next;
     }
   }
 }
 
-void print_list(){
-    proc *ptr = head;
-    printf("\n");
-    while(ptr != NULL)
-    {
-        printf("%d:    %s\n",ptr->pid,ptr->cmd);
-        ptr = ptr->next;
-    }
-    printf("Total backgound jobs: %d\n",count);
-    return;
+void print_list() {
+  proc * ptr = head;
+  printf("\n");
+  while (ptr != NULL) {
+    printf("%d:    %s\n", ptr->pid, ptr->cmd);
+    ptr = ptr->next;
+  }
+  printf("Total backgound jobs: %d\n", count);
+  return;
 }
