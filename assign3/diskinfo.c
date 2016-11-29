c62e524868110f4f165e9441ff83dca7201093b6
geofflorne <geo.lorne@gmail.com>
2016-11-28 19:54:15 -0800

    disklist done, diskinfo done except for freesize

--- assign3/diskinfo.c
+++ assign3/diskinfo.c
@@ -4,8 +4,7 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-
-
+//#include "disk.h"
 
 int get_FAT_value(int n){
 	if(n%2 == 0){
@@ -15,26 +14,25 @@ int get_FAT_value(int n){
 	}
 }
 
-char *volume_label(char *addr){
+char *volume_label(unsigned char *addr){
 	int bytes_per_sector = addr[11] + (addr[12]<<8);
-	int root = bytes_per_sector * 19;
-	int count = 0;
+	unsigned int root = bytes_per_sector * 19;
 	while(addr[root] != 0){
 		if(addr[root + 8 + 3] == 8)
 			return addr + root;
 		root += 32;
 	}
-	return "Could not find volume_label";
+	return "<Could not find volume_label>";
 }
 
-int num_root_dirs(char *addr){
+int num_root_dirs(unsigned char *addr){
 	int bytes_per_sector = addr[11] + (addr[12]<<8);
 	int root = bytes_per_sector * 19;
 	int count = 0;
+	int attr;
 	while(addr[root] != 0){
-		printf("%s      ", addr + root);
-		printf("%d\n", (addr[root + 8 + 3] & 0x10));
-		//if(addr[root + 8 + 3] & 0x10 != 0x10)
+		attr = addr[root + 8 + 3];
+		if((attr & 0x10) != 0x10 && attr != 0x0f && attr != 0x08)
 			count++;
 		root += 32;
 	}
@@ -42,7 +40,7 @@ int num_root_dirs(char *addr){
 }
 
 int main(int argc, char *argv[]){
-	char *addr;
+	unsigned char *addr;
 	int fd;
 	struct stat sb;
 
@@ -62,14 +60,12 @@ int main(int argc, char *argv[]){
 	printf("OS Name: %s\n", addr + 3);
 	printf("Label of the disk: %s\n",volume_label(addr));
 	printf("Total size of the disk: %ld bytes\n", sb.st_size);
-	//printf("Free size of the disk: %ld bytes\n", sb.st_size);
+	printf("Free size of the disk: %ld bytes\n", sb.st_size);
 	printf("\n==============\n");
 	printf("The number of files in the root directory (not including subdirectories): %d\n", num_root_dirs(addr));
 	printf("\n==============\n");
 	printf("Number of FAT copies: %d\n", addr[16]);
 	printf("Sectors per FAT : %d\n", addr[22] + (addr[23]<<8));
-
-	//printf("Bytes per sector: %d\n", addr[11] + (addr[12]<<8));
 
 	munmap(addr, sb.st_size);
 	close(fd);
--- assign3/disklist.c
+++ assign3/disklist.c
@@ -4,10 +4,78 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <string.h>
+
+unsigned char * process_name(unsigned char* name){
+	int i, j;
+	unsigned char * new_s = (unsigned char *) malloc(11);
+
+	for(i = 0; i < 8; i++){
+		if(name[i] != ' ')
+			new_s[i] = name[i];
+		else
+			break;
+	}
+
+	new_s[i] = '.';
+
+	for(j = 8; j < 11; j++){
+		if(name[j] != ' ')
+			new_s[++i] = name[j];
+		else
+			break;
+	}
+
+	return new_s;
+}
+
+void lst(unsigned char *addr){
+	int bytes_per_sector = addr[11] + (addr[12]<<8);
+	int root = bytes_per_sector * 19;
+	int size;
+	char type;
+	unsigned char * name;
+	int c_date, year, month, day;
+	int c_time, hour, minute, second;
+
+	while(addr[root] != 0){
+		if((addr[root + 8 + 3] & 0x10) == 0x10)
+			type = 'D';
+		else
+			type = 'F';
+
+		if(addr[root + 8 + 3] != 0x0f && addr[root + 8 + 3] != 0x08){
+
+			size  =  addr[root + 28] + (addr[root + 29]<<8) + (addr[root + 30]<<16) + (addr[root + 31]<<24);
+			name   =  addr + root;
+
+			if(type == 'F') name = process_name(name);
+
+			c_date =  addr[root + 16] + (addr[root + 17]<<8);
+			year  = ((c_date & 0xfe00) >> 9) + 1980;
+			month = ((c_date & 0x01e0) >> 5);
+			day   = ((c_date & 0x001f));
+
+			c_time =  addr[root + 14] + (addr[root + 15]<<8);
+			hour   = ((c_time & 0xf800) >> 11);
+			minute = ((c_time & 0x07e0) >> 5);
+			second = ((c_time & 0x001f)) * 2;
+
+			printf("%c ", type);
+			printf("%10u ", size);
+			printf("%20s ", name);
+			printf("%02d-%02d-%02d ", year, month, day);
+			printf("%02d:%02d:%02d ", hour, minute, second);
+			printf("\n");
+		}
+
+		root += 32;
+	}
+}
 
 int main(int argc, char *argv[]){
 
-	char *addr;
+	unsigned char *addr;
 	int fd;
 	struct stat sb;
 
@@ -24,9 +92,7 @@ int main(int argc, char *argv[]){
 
 	addr = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
 
-
-
-
+	lst(addr);
 	munmap(addr, sb.st_size);
 	close(fd);
 	return 0;
